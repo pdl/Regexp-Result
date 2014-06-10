9330893fc7e48140d635a6db12894e92e020353f
Daniel Perrett <perrettdl@googlemail.com>
2014-06-10 22:55:43 +0100

    _has_... methods

--- lib/Regexp/Result.pm
+++ lib/Regexp/Result.pm
@@ -18,6 +18,12 @@ Regexp::Result - store information about a regexp match for later retrieval
 
 	my $determiner = $result->c(1); # i.e. $1 at the time when the object was created
 
+Have you ever wanted to retain information about a regular expression match, without having to go through the palaver of pulling things out of C<$1>, C<pos>, etc. and assigning them each to temporary variables until you've decided what to use them as?
+
+Regexp::Result objects, when created, contain as much information about a match as perl can tell you. This means that you just need to create one variable and keep it.
+
+Hopefully, your code will be more comprehensible when it looks like C<< $result->last_numbered_match_start->[-1] >>instead of C<$-[-1]>. And unlike the punctuation variables, which are hidden away in C<perldoc perlvar> along with scary notation like C<$^H>, they are^H^H^H will be documented here, eventually with some realistic use cases.
+
 =cut
 
 has numbered_captures=>
@@ -40,6 +46,68 @@ sub c {
 	return $self->numbered_captures->[$number - 1];
     }
     return undef;
+}
+
+sub has_scalar {
+	my ($name, $creator) = @_;
+	has $name =>
+		is => 'ro',
+		default => $creator
+}
+
+#~ _has_array
+#~ 
+#~ 	_has_array primes => sub { [2,3,5,7,11] };
+#~	$object->primes->[0]; # 2
+#~	$object->primes(0);   # also 2
+
+
+sub _has_array {
+	my ($name, $creator) = @_;
+	my $realName = '_'.$name;
+	has $realName =>
+		is => 'ro',
+		default => $creator;
+	my $accessor = sub {
+		my $self = shift;
+		if (@_) {
+			#~ ideally check if @_ contains only numbers
+			#~ Should foo(1,3) return something different?
+			return $self->$realName->[@_];
+		}
+		else {
+			return $self->$realName;
+		}
+	};
+	{
+		my $package = __PACKAGE__;
+                no strict 'refs';
+                my $fullName = $package . '::' . $name;
+                *$fullName = subname( $name, $accessor );
+        }
+}
+
+sub _has_hash {
+	my ($name, $creator) = @_;
+	my $realName = '_'.$name;
+	has $realName =>
+		is => 'ro',
+		default => $creator;
+	my $accessor = sub {
+		my $self = shift;
+		if (@_) {
+			return $self->$realName->{@_};
+		}
+		else {
+			return $self->$realName;
+		}
+	};
+	{
+		my $package = __PACKAGE__;
+                no strict 'refs';
+                my $fullName = $package . '::' . $name;
+                *$fullName = subname( $name, $accessor );
+        }
 }
 
 has match =>
